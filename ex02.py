8742394adc8e19d4dbe54cb1a0ee44227eb59baf
min <mjk12300@naver.com>
2025-11-24 23:02:15 +0900

    변수, input, format 실습

---
+++ ex02.py
@@ -0,0 +1,8 @@
+# 변수 교체 swap
+
+a = input("문자열 입력 > ")
+b = input("문자열 입력 > ")
+print(a, b)
+
+(a, b) = (b, a)
+print(a, b)
---
+++ int_float.py
@@ -0,0 +1,7 @@
+input_a = float(input("첫 번째 숫자>"))
+input_b = float(input("두 번째 숫자>"))
+
+print("덧셈의 결과: ", input_a + input_b)
+print("뺄셈의 결과: ", input_a - input_b)
+print("곱셈의 결과: ", input_a * input_b)
+print("나눗셈의 결과: ", input_a / input_b)
